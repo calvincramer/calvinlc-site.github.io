b99860767ea4ec999ed77d5c07fc2d12697152ad
Calvin Lei-Cramer <calvincramer@gmail.com>
2026-01-20 12:03:27 -0600

    Book post

--- deploy.sh
+++ deploy.sh
@@ -2,31 +2,31 @@
 
 set -xue
 
-echo "Old, use github pages now"
+echo "Old, use github pages now, just git push"
 exit 1
 
-HOST=chi211.greengeeks.net
-
-if ps aww -o command | grep "jekyll serve" | grep -vq "grep"; then
-    echo "jekyll already running, will run into error when building"
-    exit 1
-fi
-
-cd "$(dirname ${BASH_SOURCE[0]})"
-
-# Build to "_site/" folder
-JEKYLL_ENV=production bundle exec jekyll build
-
-# Deploy
-tar -czf site.tar.gz -C _site/ .
-scp -q site.tar.gz calvinlc@${HOST}:/home/calvinlc
-ssh -t calvinlc@${HOST} '
-    cd /home/calvinlc &&
-    rm -rf public_html &&
-    mkdir public_html &&
-    mv site.tar.gz public_html &&
-    cd public_html &&
-    tar -xf site.tar.gz &&
-    rm site.tar.gz
-'
-echo "Good"
+# HOST=chi211.greengeeks.net
+#
+# if ps aww -o command | grep "jekyll serve" | grep -vq "grep"; then
+#     echo "jekyll already running, will run into error when building"
+#     exit 1
+# fi
+#
+# cd "$(dirname ${BASH_SOURCE[0]})"
+#
+# # Build to "_site/" folder
+# JEKYLL_ENV=production bundle exec jekyll build
+#
+# # Deploy
+# tar -czf site.tar.gz -C _site/ .
+# scp -q site.tar.gz calvinlc@${HOST}:/home/calvinlc
+# ssh -t calvinlc@${HOST} '
+#     cd /home/calvinlc &&
+#     rm -rf public_html &&
+#     mkdir public_html &&
+#     mv site.tar.gz public_html &&
+#     cd public_html &&
+#     tar -xf site.tar.gz &&
+#     rm site.tar.gz
+# '
+# echo "Good"
